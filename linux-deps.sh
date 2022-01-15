65d40fc577ee1c8b0c5b5c5c1566a3f9e2c249a1
Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>
2022-01-15 21:19:22 +0000

    update linux-deps.sh
    Signed-off-by: Gabriel Aguiar <60277618+fgsoftware1@users.noreply.github.com>

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,10 +1,4 @@
 #! /bin/sh
-apt-get update
-apt-get install -y bison flex g++ g++-multilib wget ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get install -y docker-ce docker-ce-cli containerd.io
-service docker start
-/bin/sh -c 'wget https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64 -O /usr/local/bin/earthly && chmod +x /usr/local/bin/earthly && /usr/local/bin/earthly bootstrap --with-autocomplete'
+apt-get update &&\
+apt-get upgrade -y &&\
+apt-get install -y --no-install-recommends bison flex g++ g++-multilib wget ca-certificates
