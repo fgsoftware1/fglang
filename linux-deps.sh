466be6ed33f89ccec2939e672bedd7723d12e3d3
Gabriel Aguiar <fgsoftwarestudio@gmail.com>
2022-05-31 21:41:30 +0100

    update linux-deps.sh
    Signed-off-by: Gabriel Aguiar <fgsoftwarestudio@gmail.com>

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 apt-get update &&\
 apt-get upgrade -y &&\
-apt-get install -y --no-install-recommends bison flex g++ g++-multilib wget ca-certificates
+apt-get install -y --no-install-recommends bison flex g++ g++-multilib
